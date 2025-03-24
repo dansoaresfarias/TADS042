a7fe293ef065e813488d56a93076fe428e7adf17
Danilo Farias <danilo.silva@nutes.ufpe.br>
2025-03-24 19:14:07 -0300

    Aula SQL Insert

---
+++ BDII/Pousada-Script-DML-INSERT.sql
@@ -0,0 +1,6 @@
+-- SQL: DML-INSERT
+
+insert into funcionario 
+	(cpf, nome, genero, dataNasc, email, salario, `status`)
+    value ("147.471.771-41", "Thiago Mariano", 'M', 
+    '2002-01-01', "donzelo@gmail.com", 1890.00, 1);
