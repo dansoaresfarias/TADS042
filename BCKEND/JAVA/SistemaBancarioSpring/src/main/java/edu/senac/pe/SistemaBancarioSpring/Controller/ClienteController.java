db8af8eb4bb3591d30d08457f23bfc7420b6092a
Danilo Farias <dansoaresfarias@gmail.com>
2025-11-18 21:45:16 -0300

    Api WEB

--- BCKEND/JAVA/SistemaBancarioSpring/src/main/java/edu/senac/pe/SistemaBancarioSpring/Controller/ClienteController.java
+++ BCKEND/JAVA/SistemaBancarioSpring/src/main/java/edu/senac/pe/SistemaBancarioSpring/Controller/ClienteController.java
@@ -1,9 +1,13 @@
 package edu.senac.pe.SistemaBancarioSpring.Controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import edu.senac.pe.SistemaBancarioSpring.Model.Cliente;
 import edu.senac.pe.SistemaBancarioSpring.Repository.ClienteRepository;
@@ -24,5 +28,27 @@ public class ClienteController {
 		cr.save(cliente);
 		return "cliente/salvarCliente";
 	}
+	
+	@GetMapping("/listarClientes")
+	public ModelAndView listarClientes() {
+		ModelAndView mv = new ModelAndView("cliente/listarClientes");
+		Iterable<Cliente> clientes = cr.findAll();
+		mv.addObject("clientes", clientes);		
+		return mv;
+	}
 
+	@GetMapping("/excluirCliente/{cpf}")
+	public String deletarCliente(@PathVariable String cpf) {
+		cr.deleteById(cpf);
+		return "redirect:/listarClientes";
+	}
+	
+	@GetMapping("/detalharCliente/{cpf}")
+	public ModelAndView detalharCliente(@PathVariable String cpf) {
+		ModelAndView mv = new ModelAndView("cliente/detalharCliente");
+		Optional<Cliente> cliente = cr.findById(cpf);
+		mv.addObject("cliente", cliente);
+		return mv;
+	}
+	
 }
